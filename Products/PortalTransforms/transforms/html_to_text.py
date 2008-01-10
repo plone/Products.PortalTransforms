2c136415709833d4faf1d43c6befd742af5dedc8
Florian Schulze <florian.schulze@gmx.net>
2008-01-10 13:12:32 +0000

    Implemented entity conversion in html to plain text transform.
    svn path=/Products.PortalTransforms/trunk/; revision=9236

--- Products/PortalTransforms/transforms/html_to_text.py
+++ Products/PortalTransforms/transforms/html_to_text.py
@@ -1,15 +1,29 @@
 from Products.PortalTransforms.libtransforms.retransform import retransform
 
+import htmlentitydefs
+
 class html_to_text(retransform):
     inputs  = ('text/html',)
     output = 'text/plain'
 
 def register():
-    # XXX convert entites with htmlentitydefs.name2codepoint ?
+    def sub_func(matchobj):
+        full = matchobj.group()
+        ent = matchobj.group(1)
+        result = htmlentitydefs.name2codepoint.get(ent)
+        if result is None:
+            if ent.startswith('#'):
+                return unichr(int(ent[1:]))
+            else:
+                return full
+        else:
+            return unichr(result)
+
     return html_to_text("html_to_text",
                        ('<script [^>]>.*</script>(?im)', ' '),
                        ('<style [^>]>.*</style>(?im)', ' '),
                        ('<head [^>]>.*</head>(?im)', ' '),
                        ('(?im)</?(font|em|i|strong|b)(?=\W)[^>]*>', ''),
                        ('<[^>]*>(?i)(?m)', ' '),
+                       (r'&([a-zA-Z0-9#]*?);', sub_func),
                        )
