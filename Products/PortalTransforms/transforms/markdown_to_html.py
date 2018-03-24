1a35050f41da0cfdfab31e14db83c2a24b3f3eb0
Pavel Bogdanovic <pb@prontonet.eu>
2018-03-24 11:34:38 +0100

    Move markdown configuration to registry.

--- Products/PortalTransforms/transforms/markdown_to_html.py
+++ Products/PortalTransforms/transforms/markdown_to_html.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Uses the http://www.freewisdom.org/projects/python-markdown/ module
-
 Author: Tom Lazar <tom@tomster.org> at the archipelago sprint 2006
 """
 
+from plone.registry.interfaces import IRegistry
 from Products.PortalTransforms.interfaces import ITransform
 from Products.PortalTransforms.utils import log
+from zope.component import getUtility
 from zope.interface import implementer
-
-
-import six
 
 
 try:
@@ -26,52 +24,24 @@ else:
 class markdown(object):
 
     __name__ = "markdown_to_html"
+    inputs = ("text/x-web-markdown",)
     output = "text/html"
-
-    def __init__(self,
-                 name=None,
-                 inputs=("text/x-web-markdown", ),
-                 enabled_extensions=('markdown.extensions.fenced_code',
-                                     'markdown.extensions.nl2br', ),
-                 **kwargs):
-        self.config = {
-            'inputs': inputs,
-            'enabled_extensions': enabled_extensions,
-        }
-
-        self.config_metadata = {
-            'inputs': (
-                'list',
-                'Inputs',
-                'Input(s) MIME type. Change with care.'),
-            'enabled_extensions': (
-                'list',
-                'enabled_extensions',
-                'Look for available extensions at ' +
-                'https://pythonhosted.org/Markdown/extensions/index.html ' +
-                'or write your own.'
-            ),
-        }
-
-        if name:
-            self.__name__ = name
 
     def name(self):
         return self.__name__
 
-    def __getattr__(self, attr):
-        if attr in self.config:
-            return self.config[attr]
-        raise AttributeError(attr)
+    def extensions(self):
+        registry = getUtility(IRegistry)
+        return registry.get('plone.markdown_exts')
 
     def convert(self, orig, data, **kwargs):
         if HAS_MARKDOWN:
             # markdown expects unicode input:
-            orig = six.text_type(orig.decode('utf-8'))
+            orig = unicode(orig.decode('utf-8'))
             # PortalTransforms, however expects a string as result,
             # so we encode the unicode result back to UTF8:
             html = markdown_transformer \
-                .markdown(orig, extensions=self.config.get('enabled_extensions', [])) \
+                .markdown(orig, self.extensions()) \
                 .encode('utf-8')
         else:
             html = orig
