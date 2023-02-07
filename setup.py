692cff007da57945168765c69717b8e99eb3b159
Maurits van Rees <maurits@vanrees.org>
2023-02-08 00:10:14 +0100

    Add python_requires metadata.
    [ci skip]

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(name='Products.PortalTransforms',
       namespace_packages=['Products'],
       include_package_data=True,
       zip_safe=False,
+      python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
       extras_require=dict(
           test=[
               'zope.testing',
