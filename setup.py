c8b974bd34751a81438c268c588e01cc8d067bfd
ale-rt <alessandro.pisa@gmail.com>
2023-02-10 10:30:33 +0100

    Prepare setup.py for Plone 6 and plone.base

--- setup.py
+++ setup.py
@@ -11,19 +11,14 @@ setup(
     long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
-        "Framework :: Zope :: 4",
         "Framework :: Zope :: 5",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -38,7 +33,7 @@ setup(
     namespace_packages=["Products"],
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    python_requires=">=3.8",
     extras_require=dict(
         test=[
             "zope.testing",
@@ -48,6 +43,7 @@ setup(
     install_requires=[
         "setuptools",
         "six",
+        "plone.base",
         "plone.intelligenttext",
         "zope.interface",
         "zope.structuredtext",
